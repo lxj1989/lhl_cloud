5d94b3e7dd6b53de49a35e713f33d6fda7f872ef
liuxianjun <liuxianjun1989@qq.com>
2022-11-09 10:00:33 +0800

    2022-11-09 10:00:32

--- config.js
+++ config.js
@@ -2,15 +2,15 @@ var config = {
 	database: 'lhl',
 	dialect: 'mysql',
 
-	username: 'root',
-	password: '123456',
-	host: '127.0.0.1',
-	port: 3306,
-
 	// username: 'root',
-	// password: 'dsw00000',
-	// host: 'shao5.net',
-	// port: 13306,
+	// password: '123456',
+	// host: '127.0.0.1',
+	// port: 3306,
+
+	username: 'root',
+	password: 'dsw00000',
+	host: 'shao5.net',
+	port: 13306,
 }
 
 module.exports = config
